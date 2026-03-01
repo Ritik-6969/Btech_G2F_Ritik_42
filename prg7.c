71b49492244bf8c03e49a2bf29309d2e049303a8
Ritik-6969 <25112cn274@glbitm.ac.in>
2026-03-01 18:28:14 +0530

    Add files via upload

--- prg7.c
+++ prg7.c
@@ -1,20 +1,16 @@
 #include<stdio.h>
-
-int main(){
-    int a,b,c;
-    printf("Enter a first number --->");
-    scanf("%d",&a);
-    printf("Enter a second number --->");
-    scanf("%d",&b);
-    printf("Enter third Number ---> ");
-    scanf("%d",&c);
-
-    if(a>b && a>c){
-        printf("a is greater than b & c" "%d", a);
-    }else if(b>a && b>c){
-        printf("b is greater than a & c" "%d", b);
-    }else
-        printf(" c is greater than a & b ---> " "%d",c);
-   
-    return 0;
+int main()
+{ 
+    int n;
+printf("Enter a number="),
+scanf("%d",&n);
+if(n%2==0)
+{ 
+    printf("\n n is even number.");
+}
+else
+{ 
+    printf("\n n is odd number.");
+}
+return 0;
 }
